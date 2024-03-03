da371977b5f51d4e93657dd7390ffabeb93fbd19
TomieKill88 <lordvaldomero1988@gmail.com>
2024-03-03 15:15:03 +0100

    Rotation movement added

--- Assets/Scripts/ProteinMoveControl.cs
+++ Assets/Scripts/ProteinMoveControl.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class ProteinMoveControl : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+public class ProteinMoveControl : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IPointerClickHandler
 {
+    // Objects
     private Image objImage;
     private RectTransform objRectTranform;
+
+    // Rotate variables
+    float rotateDirection;
+    bool isRotating;
+
+    // OnClick variables
+    private bool isSelected;
+    private bool wasDragged;
+
+    // OnDrag variables
     private Vector3 mouseInitPosition;
     private Vector3 objInitPosition;
 
@@ -17,31 +28,86 @@ public class ProteinMoveControl : MonoBehaviour, IDragHandler, IBeginDragHandler
     {
         objImage = GetComponent<Image>();
         objRectTranform = GetComponent<RectTransform>();
+        isSelected = false;
+        wasDragged = false;
+        isRotating = false;
     }
 
+    void Update()
+    {
+        rotateDirection = Input.GetAxisRaw("Horizontal");
+       
+        if (Mathf.Abs(rotateDirection) > Mathf.Epsilon) 
+        {
+            if (isSelected && !isRotating)
+            {
+                isRotating = true;
+                rotateObject();
+            }
+        }
+        else
+        {
+            isRotating = false;
+        }        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(!wasDragged)
+        {
+            isSelected = !isSelected;
+
+            if (isSelected)
+            {
+                objImage.color = new Color32(255, 255, 255, 170);
+            }
+            else
+            {
+                objImage.color = new Color32(255, 255, 255, 255);
+            }
+        }        
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        objImage.color = new Color32(255,255,255,170);
-        mouseInitPosition = Input.mousePosition;
-        objInitPosition = objRectTranform.localPosition;
+        if (isSelected)
+        {
+            objImage.color = new Color32(170, 170, 255, 170);
+            mouseInitPosition = Input.mousePosition;
+            objInitPosition = objRectTranform.localPosition;
+            wasDragged = true;
+        }            
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Vector3 mouseCanvasRelative = Input.mousePosition - mouseInitPosition;
-
-        objRectTranform.localPosition = objInitPosition + mouseCanvasRelative;
-
-        Debug.Log(Input.mousePosition);
-        Debug.Log("objRectTranform.position" + objRectTranform.localPosition + mouseCanvasRelative);
+        if (isSelected)
+        {
+            Vector3 mouseCanvasRelative = Input.mousePosition - mouseInitPosition;
+            objRectTranform.localPosition = objInitPosition + mouseCanvasRelative;         
+        }        
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        objImage.color = new Color32(255, 255, 255, 255);
+        if (isSelected)
+        {
+            objImage.color = new Color32(255, 255, 255, 170);
+            wasDragged = false;
+        }        
     }
 
+    public void rotateObject()
+    {
+        if (rotateDirection > 0.0f)
+        {
+            objRectTranform.Rotate(new Vector3(0.0f, 0.0f, -90.0f));
+        }
+        else
+        {
+            objRectTranform.Rotate(new Vector3(0.0f, 0.0f, 90.0f));
+        }
+    }
 
 
 }
